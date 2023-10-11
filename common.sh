d7e60f6cdca456c28cd50f852fb5a9880b4c47e2
fayaz <fayaz496@gmail.com>
2023-10-11 22:23:16 +0530

    Roboshop nginx script

--- common.sh
+++ common.sh
@@ -6,21 +6,37 @@ app_presetup()
 {
     echo -e "${color}User add${nocolor}"
   useradd roboshop &>>$log_file
-  echo $?
+   if [$? -eq 0]; then
+      echo success
+      else
+      echo Failure
+      fi
    echo -e "${color}Create Application directory${nocolor}"
    rm -rf ${app_path} &>>$log_file
      mkdir ${app_path}
-     echo $?
+      if [$? -eq 0]; then
+         echo success
+         else
+         echo Failure
+         fi
       echo -e "${color}Download application${nocolor}"
        curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>$log_file
 
        cd ${app_path}
-        echo $?
+         if [$? -eq 0]; then
+            echo success
+            else
+            echo Failure
+            fi
        echo -e "${color}Unzip File${nocolor}"
          unzip /tmp/$component.zip &>>$log_file
 
          cd ${app_path}
-          echo $?
+           if [$? -eq 0]; then
+              echo success
+              else
+              echo Failure
+              fi
    }
 nodejs()
 {
@@ -83,7 +99,11 @@ python()
   echo -e "${color} install python${nocolor}"
 
   dnf install python36 gcc python3-devel -y &>>${log_file}
-  echo $?
+  if [$? -eq 0]; then
+    echo success
+    else
+    echo Failure
+    fi
   app_presetup
 
   echo -e "${color} install dependencies${nocolor}"
