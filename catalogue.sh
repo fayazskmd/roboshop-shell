9f6542ca611e24cd432ae63c97bbe63102e19341
fayaz <fayaz496@gmail.com>
2023-10-11 15:36:53 +0530

    Roboshop nginx script

--- catalogue.sh
+++ catalogue.sh
@@ -1,31 +1,33 @@
 component=catalogue
-echo -e "\e[33mConfiguring nodejs repos\e[0m"
+color="\e[33m"
+nocolor="\e[0m"
+echo -e "${color}Configuring nodejs repos${nocolor}
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
-echo -e "\e[33mInstalling Nodejs Server\e[0m"
+echo -e "${color}Installing Nodejs Server${nocolor}
 dnf install nodejs -y &>>/tmp/roboshop.log
-echo -e "\e[33mUser add\e[0m"
+echo -e "${color}User add${nocolor}
 useradd roboshop &>>/tmp/roboshop.log
-echo -e "\e[33mCreate Application directory\e[0m"
+echo -e "${color}Create Application directory${nocolor}
 rm -rf /app &>>/tmp/roboshop.log
 mkdir /app
-echo -e "\e[33mDownload application\e[0m"
+echo -e "${color}Download application${nocolor}
 curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>/tmp/roboshop.log
 cd /app
-echo -e "\e[33mUnzip File\e[0m"
+echo -e "${color}Unzip File${nocolor}
 unzip /tmp/$component.zip &>>/tmp/roboshop.log
 cd /app
-echo -e "\e[33mInstalling dependencies\e[0m"
+echo -e "${color}Installing dependencies${nocolor}
 npm install &>>/tmp/roboshop.log
-echo -e "\e[33mUpdate setup\e[0m"
+echo -e "${color}Update setup${nocolor}
 cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service &>>/tmp/roboshop.log
-echo -e "\e[33mStart service\e[0m"
+echo -e "${color}Start service${nocolor}
 systemctl daemon-reload &>>/tmp/roboshop.log
 systemctl enable $component &>>/tmp/roboshop.log
 systemctl start $component &>>/tmp/roboshop.log
-echo -e "\e[33mCopy mongodb repo\e[0m"
+echo -e "${color}Copy mongodb repo${nocolor}
 cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
-echo -e "\e[33mInstalling mongodb client\e[0m"
+echo -e "${color}Installing mongodb client${nocolor}
 dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
-echo -e "\e[33mLoad schema\e[0m"
+echo -e "${color}Load schema${nocolor}
 mongo --host mongodb-dev.devopsb96.store </app/schema/$component.js &>>/tmp/roboshop.log
