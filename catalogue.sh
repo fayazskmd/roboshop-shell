aa0f07fde51405c6bfdb6fd49c968991083910b6
fayaz <fayaz496@gmail.com>
2023-10-11 15:24:06 +0530

    Roboshop nginx script

--- catalogue.sh
+++ catalogue.sh
@@ -1,4 +1,4 @@
-
+component=catalogue
 echo -e "\e[33mConfiguring nodejs repos\e[0m"
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
@@ -10,22 +10,22 @@ echo -e "\e[33mCreate Application directory\e[0m"
 rm -rf /app &>>/tmp/roboshop.log
 mkdir /app
 echo -e "\e[33mDownload application\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
+curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>/tmp/roboshop.log
 cd /app
 echo -e "\e[33mUnzip File\e[0m"
-unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
+unzip /tmp/$component.zip &>>/tmp/roboshop.log
 cd /app
 echo -e "\e[33mInstalling dependencies\e[0m"
 npm install &>>/tmp/roboshop.log
 echo -e "\e[33mUpdate setup\e[0m"
-cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service &>>/tmp/roboshop.log
 echo -e "\e[33mStart service\e[0m"
 systemctl daemon-reload &>>/tmp/roboshop.log
-systemctl enable catalogue &>>/tmp/roboshop.log
-systemctl start catalogue &>>/tmp/roboshop.log
+systemctl enable $component &>>/tmp/roboshop.log
+systemctl start $component &>>/tmp/roboshop.log
 echo -e "\e[33mCopy mongodb repo\e[0m"
 cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 echo -e "\e[33mInstalling mongodb client\e[0m"
 dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
 echo -e "\e[33mLoad schema\e[0m"
-mongo --host mongodb-dev.devopsb96.store </app/schema/catalogue.js &>>/tmp/roboshop.log
+mongo --host mongodb-dev.devopsb96.store </app/schema/$component.js &>>/tmp/roboshop.log
