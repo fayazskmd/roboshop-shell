8a566fdd1d8f2e36545de995a1a236735ee0ee36
fayaz <fayaz496@gmail.com>
2023-10-11 17:25:13 +0530

    Roboshop nginx script

--- common.sh
+++ common.sh
@@ -2,7 +2,20 @@ color="\e[35m"
 nocolor="\e[0m"
 log_file="/tmp/roboshop.log"
 app_path="/app"
-
+app_presetup()
+{
+    echo -e "${color}User add${nocolor}"
+  useradd roboshop &>>$log_file
+   echo -e "${color}Create Application directory${nocolor}"
+   rm -rf ${app_path} &>>$log_file
+     mkdir ${app_path}
+      echo -e "${color}Download application${nocolor}"
+       curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>$log_file
+       cd ${app_path}
+       echo -e "${color}Unzip File${nocolor}"
+         unzip /tmp/$component.zip &>>$log_file
+         cd ${app_path}
+   }
 nodejs()
 {
   echo -e "${color}Configuring nodejs repos${nocolor}"
@@ -10,17 +23,11 @@ nodejs()
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
   echo -e "${color}Installing Nodejs Server${nocolor}"
   dnf install nodejs -y &>>$log_file
-  echo -e "${color}User add${nocolor}"
-  useradd roboshop &>>$log_file
-  echo -e "${color}Create Application directory${nocolor}"
-  rm -rf ${app_path} &>>$log_file
-  mkdir ${app_path}
-  echo -e "${color}Download application${nocolor}"
-  curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>$log_file
-  cd ${app_path}
-  echo -e "${color}Unzip File${nocolor}"
-  unzip /tmp/$component.zip &>>$log_file
-  cd ${app_path}
+
+  app_presetup
+
+
+
   echo -e "${color}Installing dependencies${nocolor}"
   npm install &>>$log_file
   echo -e "${color}Update setup${nocolor}"
@@ -41,3 +48,27 @@ mongo_schema_setup()
   mongo --host mongodb-dev.devopsb96.store <${app_path}/schema/$component.js &>>${log_file}
 }
 
+maven()
+{
+
+echo -e "${color} install maven${nocolor}"
+dnf install maven -y &>>${log_file}
+app_presetup
+
+echo -e "${color} cleaned package${nocolor}"
+mvn clean package &>>${log_file}
+mv target/shipping-1.0.jar shipping.jar &>>${log_file}
+
+echo -e "${color} install mysql${nocolor}"
+dnf install mysql -y &>>${log_file}
+echo -e "${color} mysql client${nocolor}"
+mysql -h mysql-dev.devopsb96.store -uroot -pRoboShop@1 <${app_path}/schema/shipping.sql &>>${log_file}
+echo -e "${color} system setup${nocolor}"
+cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service
+
+echo -e "${color} start shipping${nocolor}"
+systemctl daemon-reload &>>${log_file}
+
+systemctl enable shipping &>>${log_file}
+systemctl restart shipping &>>${log_file}
+}
--- shipping.sh
+++ shipping.sh
@@ -1,36 +1,8 @@
 
 
+source common.sh
+component=shipping
 
-echo -e "\e[33m install maven\e[0m"
-dnf install maven -y &>>/tmp/roboshop.log
-echo -e "\e[33m user add\e[0m"
-useradd roboshop &>>/tmp/roboshop.log
-echo -e "\e[33m update app\e[0m"
-rm -rf /app &>>/tmp/roboshop.log
-mkdir /app
-echo -e "\e[33m download application\e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>>/tmp/roboshop.log
-
-echo -e "\e[33m unzip file\e[0m"
-cd /app
-unzip /tmp/shipping.zip &>>/tmp/roboshop.log
-
-
-echo -e "\e[33m cleaned package\e[0m"
-mvn clean package &>>/tmp/roboshop.log
-mv target/shipping-1.0.jar shipping.jar &>>/tmp/roboshop.log
-
-echo -e "\e[33m install mysql\e[0m"
-dnf install mysql -y &>>/tmp/roboshop.log
-echo -e "\e[33m mysql client\e[0m"
-mysql -h mysql-dev.devopsb96.store -uroot -pRoboShop@1 </app/schema/shipping.sql &>>/tmp/roboshop.log
-echo -e "\e[33m system setup\e[0m"
-cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service
-
-echo -e "\e[33m start shipping\e[0m"
-systemctl daemon-reload &>>/tmp/roboshop.log
-
-systemctl enable shipping &>>/tmp/roboshop.log
-systemctl restart shipping &>>/tmp/roboshop.log
+maven
 
 
