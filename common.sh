954086a7a8648743a369a6de28ed059f70a9aa0c
fayaz <fayaz496@gmail.com>
2023-10-11 17:03:03 +0530

    Roboshop nginx script

--- common.sh
+++ common.sh
@@ -31,13 +31,13 @@ nodejs()
   systemctl start $component &>>$log_file
 }
 
-mongo_schema_setup ()
+mongo_schema_setup()
 {
-  echo -e "\${color}Copy mongodb repo${nocolor}"
+  echo -e "${color}Copy mongodb repo${nocolor}"
   cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
-  echo -e "\${color}Installing mongodb client${nocolor}"
+  echo -e "${color}Installing mongodb client${nocolor}"
   dnf install mongodb-org-shell -y &>>${log_file}
-  echo -e "\${color}Load schema${nocolor}"
+  echo -e "${color}Load schema${nocolor}"
   mongo --host mongodb-dev.devopsb96.store <${app_path}/schema/$component.js &>>${log_file}
 }
 
